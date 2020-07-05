49e2d7281ddc70e8082f170a002c749dbd286f02
Gagan61 <gagan.goyal61@gmail.com>
2020-07-05 02:14:43 -0700

    Logic of javascript working

--- colorGame.js
+++ colorGame.js
@@ -1,18 +1,64 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)"
-];
+var numSquare = 6;
+var colors = generateRandomColor(numSquare);
 
 var squares = document.querySelectorAll(".square");
 var colorDisp = document.getElementById("colorDisp");
 var pickedColor = pickColor();
-colorDisp.textContent =  pickedColor;
+
 var clickedColor;
 var messageDisp = document.getElementById("message");
+var h1 = document.querySelector("h1") ;
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+colorDisp.textContent =  pickedColor; //change the color in header
+
+easyBtn.addEventListener("click",function(){
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    numSquare = 3;
+    colors = generateRandomColor(numSquare);
+    pickedColor  = pickColor();
+    colorDisp.textContent =  pickedColor; 
+    //remove bottom three and change upper three
+    for( var i=0; i<squares.length ; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        } else{
+            squares[i].style.display = "none";
+        }
+    } 
+});
+hardBtn.addEventListener("click",function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numSquare = 6;
+    colors = generateRandomColor(numSquare);
+    pickedColor  = pickColor();
+    colorDisp.textContent =  pickedColor; 
+    //remove bottom three and change upper three
+    for( var i=0; i<squares.length ; i++){
+        {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+        } 
+    } 
+})
+
+resetButton.addEventListener("click", function(){
+    //generate all colors
+    colors = generateRandomColor(numSquare);
+    //pick color
+    pickedColor = pickColor(); 
+    //change color in header
+    colorDisp.textContent =  pickedColor; 
+    //change color of squares
+    for( var i=0; i<squares.length ; i++){
+        squares[i].style.backgroundColor = colors[i];
+    }    
+    h1.style.background = "#232323";
+
+});
 
 for( var i=0; i<squares.length ; i++){
     squares[i].style.backgroundColor = colors[i];
@@ -26,6 +72,8 @@ for( var i=0; i<squares.length ; i++){
         if(clickedColor === pickedColor){
             messageDisp.textContent = "Correct"
             changeColor(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play Again ?"
         } 
         else {
             //alert("wrong");
@@ -36,7 +84,7 @@ for( var i=0; i<squares.length ; i++){
     });
 }
 
-function changeColor (color){
+function changeColor(color){
     // change color of all squares
     //loop through all squares
     for (var i=0; i<squares.length; i++){
@@ -45,6 +93,21 @@ function changeColor (color){
 }
  
 function pickColor (){
-    var random = Math.floor(Math.random()* colors.length);
+    var random = Math.floor(Math.random() * colors.length);
     return (colors[random]);
+}
+
+function generateRandomColor (num){
+    var arr = [];
+    for  (var i=0; i<num; i++){
+        arr[i]= randomColor();
+    }
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
