3baf062e832ba7dbe4ccd7a325e4b4183785946a
Gagan61 <gagan.goyal61@gmail.com>
2020-07-05 00:47:49 -0700

    Colors pick and changed

--- colorGame.js
+++ colorGame.js
@@ -9,9 +9,10 @@ var colors = [
 
 var squares = document.querySelectorAll(".square");
 var colorDisp = document.getElementById("colorDisp");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 colorDisp.textContent =  pickedColor;
 var clickedColor;
+var messageDisp = document.getElementById("message");
 
 for( var i=0; i<squares.length ; i++){
     squares[i].style.backgroundColor = colors[i];
@@ -22,7 +23,28 @@ for( var i=0; i<squares.length ; i++){
         console.log(clickedColor);
 
     //compare clicked color with picked color
-        if(clickedColor === pickedColor) alert("correct");
-        else alert("wrong");
+        if(clickedColor === pickedColor){
+            messageDisp.textContent = "Correct"
+            changeColor(clickedColor);
+        } 
+        else {
+            //alert("wrong");
+            // change clicked color to body color
+            this.style.backgroundColor = "#232323";
+            messageDisp.textContent = "Try Again";
+        }
     });
+}
+
+function changeColor (color){
+    // change color of all squares
+    //loop through all squares
+    for (var i=0; i<squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
+ 
+function pickColor (){
+    var random = Math.floor(Math.random()* colors.length);
+    return (colors[random]);
 }
